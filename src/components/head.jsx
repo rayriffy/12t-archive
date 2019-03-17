a9330c4ed11ba15fb89222bb4b18d4c6501b0c05
Phumrapee Limpianchop <git@rayriffy.com>
2019-03-17 22:12:32 +0700

    :sparkles: Complete head replica

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React from 'react'
 import PropTypes from 'prop-types'
 import {StaticQuery, graphql, Link} from 'gatsby'
@@ -10,50 +11,60 @@ export default class LayoutTemplate extends React.Component {
   render() {
     const {children} = this.props
     return (
-      <div>
-        <div className={headStyle.logo}>
-          <Link to="/">
-            <i className={headStyle.image}></i>
-          </Link>
-        </div>
-        <div className={headStyle.character}>
-          <a href="" target="_self" title="หน้าหลัก">
-            <div id="randomImage">
-              <StaticQuery
-                query={graphql`
-                  query HeadLogoQuery {
-                    allFile(filter: {relativeDirectory: {eq: "images\\character"}}) {
-                      edges {
-                        node {
-                          childImageSharp {
-                            fluid(maxWidth: 680, quality: 90) {
-                              base64
-                              tracedSVG
-                              aspectRatio
-                              src
-                              srcSet
-                              srcWebp
-                              srcSetWebp
-                              sizes
-                            }
-                          }
-                        }
-                      }
-                    }
+      <StaticQuery
+      query={graphql`
+        query HeadComponentQuery {
+          file(relativePath: {eq: "images/logo/12tlogo.png"}) {
+            childImageSharp {
+              fluid(maxWidth: 480, quality: 90) {
+                base64
+                tracedSVG
+                aspectRatio
+                src
+                srcSet
+                sizes
+              }
+            }
+          }
+          allFile(filter: {relativePath: {regex: "/(images/character)/"}}) {
+            edges {
+              node {
+                childImageSharp {
+                  fluid(maxWidth: 680, quality: 90) {
+                    base64
+                    tracedSVG
+                    aspectRatio
+                    src
+                    srcSet
+                    sizes
                   }
-                `}
-                render={data => {
-                  _.each(data.allFile.edges, edge => {
-                    return (
-                      <Img fluid={edge.node.childImageSharp.fluid} />
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <div className={headStyle.head}>
+          <div className={headStyle.logo}>
+            <Link to="/">
+              <Img fluid={data.file.childImageSharp.fluid} className={headStyle.image} />
+            </Link>
+          </div>
+          <div className={headStyle.character}>
+            <a href="" target="_self" title="หน้าหลัก">
+              <div id="randomImage">
+                <Img fluid={data.allFile.edges[Math.floor(Math.random() * data.allFile.edges.length)].node.childImageSharp.fluid} />
+                  {/* {data.allFile.edges.map(({node}) => (
+                      
                     )
-                  })
-                }}
-              />
-            </div>
-          </a>
+                  )} */}
+              </div>
+            </a>
+          </div>
         </div>
-      </div>
+      )}
+    />
     )
   }
 }
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import {graphql} from 'gatsby'
+
+import Layout from '../components/layout'
+
+export default class IndexPage extends React.Component {
+  render() {
+
+    return (
+      <Layout>
+      </Layout>
+    )
+  }
+}
